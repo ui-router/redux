11d7cf0332edeba8bd7d4b581d308c7544dbfc41
Marco Botto <marco.botto@gmail.com>
2017-10-31 22:16:07 +0000

    fix(webpack): add externals and react-based bundle

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ var webpack = require("webpack");
 var config = {
   entry: {
     "uirouter-redux": ["./core/index.ts"],
-    "uirouter-redux.min": ["./core/index.ts"]
+    "uirouter-redux.min": ["./core/index.ts"],
+    "uirouter-redux-react": ["./react/index.ts"],
+    "uirouter-redux-react.min": ["./react/index.ts"],
   },
   output: {
     path: path.resolve(__dirname, "_bundles"),
@@ -31,6 +33,12 @@ var config = {
         exclude: /(node_modules|__tests__)/
       }
     ]
+  },
+  externals: {
+    "react": { root: 'React', amd: 'react', commonjs2: 'react', commonjs: 'react' },
+    "prop-types": { root: 'PropTypes', amd: 'prop-types', commonjs2: 'prop-types', commonjs: 'prop-types' },
+    "@uirouter/core": { root: 'UIRouter', amd: '@uirouter/core', commonjs2: '@uirouter/core', commonjs: '@uirouter/core' },
+    "@uirouter/react": { root: 'UIRouterReact', amd: '@uirouter/react', commonjs2: '@uirouter/react', commonjs: '@uirouter/react' }
   }
 };
 
