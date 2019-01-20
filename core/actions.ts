96b14de8e4d73f709e7001734282bacbe38eac52
Law Smith <lawsumisu@gmail.com>
2019-01-20 13:36:55 -0800

    Added SUCCESS_TRANSITION

--- core/actions.ts
+++ core/actions.ts
@@ -3,6 +3,7 @@ export const START_TRANSITION = '@ui-router/START_TRANSITION';
 export const IGNORED_TRANSITION = '@ui-router/IGNORED_TRANSITION';
 export const REDIRECTED_TRANSITION = '@ui-router/REDIRECTED_TRANSITION';
 export const FINISH_TRANSITION = '@ui-router/FINISH_TRANSITION';
+export const SUCCESS_TRANSITION = '@ui-router/SUCCESS_TRANSITION';
 
 export const triggerTransition = (to, params) => {
   return {
